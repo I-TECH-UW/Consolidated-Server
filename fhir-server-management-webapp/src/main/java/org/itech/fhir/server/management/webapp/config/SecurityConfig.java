e527ccc3042376890460817e09329bddff256579
CalebSLane <c.steelelane@gmail.com>
2020-06-29 13:57:53 -0700

    fix authentication not being properly applied

--- fhir-server-management-webapp/src/main/java/org/itech/fhir/server/management/webapp/config/SecurityConfig.java
+++ fhir-server-management-webapp/src/main/java/org/itech/fhir/server/management/webapp/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package org.itech.fhir.server.management.webapp.config;
 
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
 import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -18,13 +20,14 @@ import lombok.extern.slf4j.Slf4j;
 
 @Configuration
 @EnableWebSecurity
+@ComponentScan(basePackageClasses = KeycloakSecurityComponents.class)
 @Slf4j
 public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		super.configure(http.antMatcher("/api/**"));
-		http.antMatcher("/api/**").authorizeRequests().antMatchers("/api*").hasRole("user").anyRequest().permitAll();
+		super.configure(http);
+		http.authorizeRequests().antMatchers("/api/**").hasRole("user").anyRequest().permitAll();
 	}
 
 	@Autowired
